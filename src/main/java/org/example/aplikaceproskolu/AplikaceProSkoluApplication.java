f2f022042727f51d943313747249dee6546594f1
Samuel Huf <samhuf@seznam.cz>
2024-09-18 07:48:00 +0200

    Initial commit

---
+++ src/main/java/org/example/aplikaceproskolu/AplikaceProSkoluApplication.java
@@ -0,0 +1,13 @@
+package org.example.aplikaceproskolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikaceProSkoluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikaceProSkoluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/aplikaceproskolu/AplikaceProSkoluApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aplikaceproskolu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikaceProSkoluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
