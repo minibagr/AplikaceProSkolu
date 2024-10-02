f59a19572b4886b0bf2d081b80f8c043e590216b
Samuel Huf <samhuf@seznam.cz>
2024-10-02 08:05:11 +0200

    k

--- src/main/java/org/example/aplikaceproskolu/AplikaceProSkoluApplication.java
+++ src/main/java/org/example/aplikaceproskolu/AplikaceProSkoluApplication.java
@@ -2,12 +2,18 @@ package org.example.aplikaceproskolu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AplikaceProSkoluApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplikaceProSkoluApplication.class, args);
+	}
+
+	@GetMapping("/")
+	String profile() {
+		return "profile.html";
 	}
 
 }
