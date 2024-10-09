8a620714ba04239d3680fbf4b2b345601be14c9b
Samuel Huf <samhuf@seznam.cz>
2024-10-09 08:00:50 +0200

    controller

--- src/main/java/org/example/aplikaceproskolu/NewController.java
+++ src/main/java/org/example/aplikaceproskolu/NewController.java
@@ -1,14 +1,33 @@
 package org.example.aplikaceproskolu;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 @Controller
 public class NewController {
 
     @GetMapping("/")
-    public String profile() {
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable UUID teacherUUID, Model model) {
         return "profile";
+    }
+
+    @GetMapping("/problemy")
+    public String problemy() {
+        return "problemy";
+    }
+
+    @GetMapping("/problem/{id}")
+    public String problem(@PathVariable UUID problemUUID, Model model) {
+        return "problem";
     }
 }
